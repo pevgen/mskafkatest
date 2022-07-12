e20c6fddfe5de997d039c72131f7a8791b3a7619
Evgeny Polyak <epolyak@luxoft.com>
2022-07-12 18:55:06 +0300

    Initial commit

---
+++ consumer/src/test/java/ml/pevgen/mskafkatest/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.pevgen.mskafkatest.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ producer/src/test/java/ml/pevgen/mskafkatest/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.pevgen.mskafkatest.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
